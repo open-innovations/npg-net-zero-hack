551049f7b1ea30f00dad8bb425db9a51b17522ed
slowe <299787+slowe@users.noreply.github.com>
2024-01-31 16:51:30 +0000

    Update example

--- _config.ts
+++ _config.ts
@@ -6,7 +6,7 @@ import postcss from "lume/plugins/postcss.ts";
 import { walkSync } from 'std/fs/mod.ts';
 
 // Importing the OI Lume charts and utilities
-import oiCharts from "https://deno.land/x/oi_lume_viz@v0.13.13/mod.ts";
+import oiCharts from "../oi-lume-viz/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.3.0/processors/auto-dependency.ts";
 import csvLoader from "https://deno.land/x/oi_lume_utils@v0.3.0/loaders/csv-loader.ts";
 import jsonLoader from "lume/core/loaders/json.ts";
